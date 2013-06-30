81504c680e52f7ff2c2cad3e1a9835ed0c398eb0
caopeiran <peirancao@gmail.com>
2013-06-30 12:52:01 +0800

    Signed-off-by: caopeiran <peirancao@gmail.com>

--- seajs-debug/assets/js/app/foo/1.0.0/foo.js
+++ seajs-debug/assets/js/app/foo/1.0.0/foo.js
@@ -1,8 +1,7 @@
 define(function(require) {
     var $ = require('jquery');
     
-    var Widget = require("arale/widget/1.0.3/widget");
-    var Templatable = require("arale/widget/1.0.3/templatable");
+    var Widget = require("arale/widget/1.0.2/widget");
+    var Templatable = require("arale/widget/1.0.2/templatable");
     var tpl = require("./foo.tpl");
-    var Calendar = require("arale/calendar/0.8.5/calendar");
 });
