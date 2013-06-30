ee391b57ee82f2860bfd60c0c4f5fa8f96ba9df9
caopeiran <peirancao@gmail.com>
2013-06-30 12:48:40 +0800

    Signed-off-by: caopeiran <peirancao@gmail.com>

---
+++ seajs-debug/assets/js/app/foo/1.0.0/foo.js
@@ -0,0 +1,8 @@
+define(function(require) {
+    var $ = require('jquery');
+    
+    var Widget = require("arale/widget/1.0.3/widget");
+    var Templatable = require("arale/widget/1.0.3/templatable");
+    var tpl = require("./foo.tpl");
+    var Calendar = require("arale/calendar/0.8.5/calendar");
+});
